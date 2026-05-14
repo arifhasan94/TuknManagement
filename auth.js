c66ac6a945f26fc59b024cd6d85ac8b60f15411d
Arif Hasan <sm.arifhasan09@gmail.com>
2026-05-14 15:50:38 +0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,1 +1,22 @@
 
+async function login() {
+  const id = document.getElementById("loginId").value;
+  const pass = document.getElementById("password").value;
+
+  const user = fakeDB.find(u =>
+    (u.member_id === id || u.mobile === id) && u.password === pass
+  );
+
+  if (!user) {
+    document.getElementById("error").innerText = "Invalid login";
+    return;
+  }
+
+  localStorage.setItem("user", JSON.stringify(user));
+
+  if (user.role === "admin" || user.role === "super_admin") {
+    window.location.href = "admin.html";
+  } else {
+    window.location.href = "user.html";
+  }
+}
