a806048554895132b8a2cb11408929822a46adbe
Arif Hasan <sm.arifhasan09@gmail.com>
2026-05-14 17:04:08 +0600

    Update user.js

--- user.js
+++ user.js
@@ -1,1 +1,15 @@
+let currentUser = JSON.parse(localStorage.getItem("user"));
 
+// ইউজার ডাটা লোড
+function loadUser() {
+  if (!currentUser) {
+    window.location.href = "index.html";
+    return;
+  }
+
+  document.getElementById("userName").innerText = currentUser.name;
+  document.getElementById("memberId").innerText = currentUser.member_id;
+
+  loadBalance();
+  loadHistory();
+}
